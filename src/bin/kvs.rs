b7e89fb983c4741c438f05adbf51fe40ddbc8d70
DeadGuyswitch <nirdesh.ghost@gmail.com>
2022-08-05 08:52:38 +0545

    No arguments test and version test passed

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,3 +1,23 @@
+use clap::{Command, Arg};
+
 fn main() {
-    println!("In the beninging!");
+    let _matches = Command::new("kvs")
+        .version("0.1.0")
+        .author("Nirdesh Bhattarai nbhattarai.work@gmail.com")
+        .about("A simple in-memory key value store in Rust")
+        .arg_required_else_help(true)
+        .arg(
+            Arg::new("version")
+                .short('V')
+                .long("version")
+                .action(clap::ArgAction::Version)
+        )
+        // .arg(
+        //     Arg::new("get")
+        //         .short('g')
+        //         .long("get")
+        //         .action(action)
+
+        // )
+        .get_matches();
 }
