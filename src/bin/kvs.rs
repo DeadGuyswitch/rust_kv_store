09d450a5c16fca93857382d590fdcbbe308c1420
DeadGuyswitch <nirdesh.ghost@gmail.com>
2022-08-11 22:39:32 +0545

    Match get and set arguments

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,23 +1,50 @@
-use clap::{Command, Arg};
+use clap::{Command, SubCommand, Arg};
 
 fn main() {
-    let _matches = Command::new("kvs")
+    let matches = Command::new("kvs")
         .version("0.1.0")
         .author("Nirdesh Bhattarai nbhattarai.work@gmail.com")
         .about("A simple in-memory key value store in Rust")
         .arg_required_else_help(true)
+        .subcommand(
+    SubCommand::with_name("get")
+                .about("Get value for a specified key")
+                .arg(
+                    Arg::with_name("key")
+                        .takes_value(true)
+                        .value_name("key")
+                        .required(true)
+                )
+        )
+        .subcommand(
+            SubCommand::with_name("set")
+                        .about("Set key:value")
+                        .arg(
+                            Arg::with_name("key")
+                                .takes_value(true)
+                                .value_name("key")
+                                .required(true)
+                        )
+                        .arg(
+                            Arg::with_name("value")
+                                .takes_value(true)
+                                .value_name("value")
+                                .required(true)
+                        )
+                )
         .arg(
             Arg::new("version")
                 .short('V')
                 .long("version")
                 .action(clap::ArgAction::Version)
         )
-        // .arg(
-        //     Arg::new("get")
-        //         .short('g')
-        //         .long("get")
-        //         .action(action)
+        .get_matches();  
+    
+    if let Some(c) = matches.subcommand_matches("get") {
+        eprintln!("unimplemented");
+    }
 
-        // )
-        .get_matches();
+    if let Some(c) = matches.subcommand_matches("set") {
+        eprintln!("unimplemented");
+    }
 }
